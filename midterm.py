c9f5ea84d6d7c9df223434a624773351ee5d3862
punpunbaba <punbababa@gmail.com>
2023-12-21 11:55:03 +0700

    add

--- midterm.py
+++ midterm.py
@@ -1,7 +1,6 @@
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.tree import plot_tree
 import pandas as pd
 import numpy as np
 
